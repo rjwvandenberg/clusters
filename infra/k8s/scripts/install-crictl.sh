110c93876c5736aea61a20bab9f342b63737ad2d
René van den Berg <rjwvandenberg@gmail.com>
2024-04-06 16:48:49 +0200

    Fix directory access modes and crictl install

--- infra/k8s/scripts/install-crictl.sh
+++ infra/k8s/scripts/install-crictl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION="v1.29.0" # check latest version in /releases page
-curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz --output crictl-$VERSION-linux-amd64.tar.gz
-sudo tar -zxvf --no-same-owner crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+curl -L --fail --remote-name-all https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin --no-same-owner -zxvf crictl-$VERSION-linux-amd64.tar.gz
 rm -f crictl-$VERSION-linux-amd64.tar.gz
