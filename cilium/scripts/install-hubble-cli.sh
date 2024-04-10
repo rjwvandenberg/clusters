b0f0a9f6846c64d6e54fa9bebf6ebae4fcdde605
René van den Berg <rjwvandenberg@gmail.com>
2024-04-10 19:05:52 +0200

    Fix aarch -> arm conversion

--- cilium/scripts/install-hubble-cli.sh
+++ cilium/scripts/install-hubble-cli.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 HUBBLE_VERSION="v0.13.2"
-if [ "$(uname -m)" = "aarch64" ]; then ARCH=arm64; fi
 curl -L --fail --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-linux-${ARCH}.tar.gz{,.sha256sum}
 sha256sum --check hubble-linux-${ARCH}.tar.gz.sha256sum
 sudo tar -C /usr/local/bin --no-same-owner -xzvf hubble-linux-${ARCH}.tar.gz
